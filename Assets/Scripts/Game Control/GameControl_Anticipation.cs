05e91173a60b7a055513a436a12f9e0cb3f8d65f
domasgim <38397825+domasgim@users.noreply.github.com>
2022-04-05 20:27:15 +0300

    anticipation connected menu, still not working buttons

--- Assets/Scripts/Game Control/GameControl_Anticipation.cs
+++ Assets/Scripts/Game Control/GameControl_Anticipation.cs
@@ -6,15 +6,28 @@ using System;
 using PathCreation;
 using PathCreation.Examples;
 using UnityEngine;
+using TMPro;
 using Random = System.Random;
 
 public class GameControl_Anticipation : MonoBehaviour
 {
     [SerializeField]
+    public GameObject playerFollowCamera;
+
+    [SerializeField]
     private GameObject resultsPanel;
 
     [SerializeField]
-    private Text scoreText, targetsHitText, shotsFiredText, accuracyText, currentTimeText;
+    private GameObject finalResultsPanel;
+
+    //[SerializeField]
+    //private Text scoreText, targetsHitText, shotsFiredText, accuracyText, currentTimeText;
+
+    [SerializeField]
+    private TextMeshProUGUI finalScoreText, finalTargetsHitText, finalShotsFiredText, finalAccuracyText, finalCurrentTimeText;
+
+    [SerializeField]
+    private TextMeshProUGUI currentTimeText, targetsHitText;
 
     [SerializeField]
     Camera cam;
@@ -61,9 +74,16 @@ public class GameControl_Anticipation : MonoBehaviour
         TARGET_STOP
     }
 
+    public enum pauseStatusEnum
+    {
+        PAUSED,
+        RESUMED
+    }
+
     public gameStatusEnum gameStatus = gameStatusEnum.STANDBY;
     public targetStatusEnum targetStatus = targetStatusEnum.TARGETS_DISABLED;
     public targetMoveStatusEnum targetMoveStatus = targetMoveStatusEnum.TARGET_STOP;
+    public pauseStatusEnum pauseStatus = pauseStatusEnum.RESUMED;
 
     // Start is called before the first frame update
     void Start()
@@ -77,102 +97,116 @@ public class GameControl_Anticipation : MonoBehaviour
         accuracy = 0f;
 
         resultsPanel.SetActive(true);
-        scoreText.text = "Score: " + score;
+        finalResultsPanel.SetActive(false);
+        //scoreText.text = "Score: " + score;
         targetsHitText.text = "Targets Hit: " + targetsHit + "/" + targetsAmmountInitial;
-        shotsFiredText.text = "Shots Fired: " + shotsFired;
-        accuracyText.text = "Accuracy: " + accuracy.ToString("N2") + " %";
+        //shotsFiredText.text = "Shots Fired: " + shotsFired;
+        //accuracyText.text = "Accuracy: " + accuracy.ToString("N2") + " %";
         currentTimeText.text = "Time: -";
     }
 
     // Update is called once per frame
     void Update()
     {
-        PrintTime();
-
-        // Mouse 1 pressed
-        if (Input.GetMouseButtonDown(0))
+        if (pauseStatus != pauseStatusEnum.PAUSED)
         {
-            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                Target_Anticipation target = hit.collider.gameObject.GetComponent<Target_Anticipation>();
+            PrintTime();
 
-                // A target is hit
-                if (target != null)
+            // Mouse 1 pressed
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+                if (Physics.Raycast(ray, out RaycastHit hit))
                 {
-                    if (gameStatus == gameStatusEnum.STANDBY)
+                    Target_Anticipation target = hit.collider.gameObject.GetComponent<Target_Anticipation>();
+
+                    // A target is hit
+                    if (target != null)
                     {
-                        gameStatus = gameStatusEnum.STARTED;
-                        target.Hit(hit.point);
-                        SpawnRandomPath();
-                        DestroyTrigger();
-                    }
-                    else if (gameStatus == gameStatusEnum.STARTED)
-                    {
-                        int hitReturn = 0;
-                        if ((hitReturn = target.Hit(hit.point)) != 0)
+                        if (gameStatus == gameStatusEnum.STANDBY)
                         {
-                            // TODO: save time here
-                            // restart timer
-                            currentTime = 0f;
-                            if (targetsHit < targetsAmmountInitial)
+                            gameStatus = gameStatusEnum.STARTED;
+                            target.Hit(hit.point);
+                            SpawnRandomPath();
+                            DestroyTrigger();
+                        }
+                        else if (gameStatus == gameStatusEnum.STARTED)
+                        {
+                            int hitReturn = 0;
+                            if ((hitReturn = target.Hit(hit.point)) != 0)
                             {
-                                score += hitReturn;
-                                targetsHit++;
-                                targetNumber++;
-                                if (targetsHit == targetsAmmountInitial)
+                                // TODO: save time here
+                                // restart timer
+                                currentTime = 0f;
+                                if (targetsHit < targetsAmmountInitial)
                                 {
-                                    shotsFired++;
-                                    gameStatus = gameStatusEnum.FINISHED;
-                                    targetStatus = targetStatusEnum.TARGETS_DISABLED;
-                                    target.DisableTarget();
-                                } else
-                                {
-                                    shotsFired++;
-                                    SpawnRandomPath();
+                                    score += hitReturn;
+                                    targetsHit++;
+                                    targetNumber++;
+                                    if (targetsHit == targetsAmmountInitial)
+                                    {
+                                        shotsFired++;
+                                        gameStatus = gameStatusEnum.FINISHED;
+                                        targetStatus = targetStatusEnum.TARGETS_DISABLED;
+                                        target.DisableTarget();
+                                    }
+                                    else
+                                    {
+                                        shotsFired++;
+                                        SpawnRandomPath();
+                                    }
                                 }
-                            } 
+                            }
                         }
                     }
                 }
+                if (targetStatus == targetStatusEnum.TARGETS_ENABLED)
+                {
+                    shotsFired++;
+                }
+                PrintResults();
             }
-            if (targetStatus == targetStatusEnum.TARGETS_ENABLED)
+            if (currentTime > singleTargetLifeTime)
             {
-                shotsFired++;
+                targetNumber++;
+                currentTime = 0f;
+                targetStatus = GameControl_Anticipation.targetStatusEnum.TARGETS_DISABLED;
+                SpawnRandomPath();
             }
-            PrintResults();
-        }
-        if (currentTime > singleTargetLifeTime)
+            if (targetNumber == targetsAmmountInitial)
+            {
+                pauseStatus = pauseStatusEnum.PAUSED;
+                gameStatus = gameStatusEnum.FINISHED;
+                targetStatus = targetStatusEnum.TARGETS_DISABLED;
+            }
+        } else if (gameStatus == gameStatusEnum.FINISHED)
         {
-            targetNumber++;
-            currentTime = 0f;
-            targetStatus = GameControl_Anticipation.targetStatusEnum.TARGETS_DISABLED;
-            SpawnRandomPath();
-        }
-
-        if (targetNumber == targetsAmmountInitial)
-        {
-            gameStatus = gameStatusEnum.FINISHED;
-            targetStatus = targetStatusEnum.TARGETS_DISABLED;
-        }
-
-        if (gameStatus == gameStatusEnum.FINISHED)
-        {
-            Debug.Log("GAME OVER");
+            Debug.Log("VISKAS");
+            resultsPanel.SetActive(false);
+            finalResultsPanel.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+            playerFollowCamera.SetActive(false);
+            PrintTime();
         }
     }
 
     private void PrintResults()
     {
-        scoreText.text = "Score: " + score;
+        //scoreText.text = "Score: " + score;
         targetsHitText.text = "Targets Hit: " + targetsHit + "/" + targetsAmmountInitial;
-        shotsFiredText.text = "Shots Fired: " + shotsFired;
+        //shotsFiredText.text = "Shots Fired: " + shotsFired;
         // Else at the start, 'NaN' would be printed out
         if (shotsFired > 0)
         {
             accuracy = targetsHit / shotsFired * 100f;
         }
-        accuracyText.text = "Accuracy: " + accuracy.ToString("N2") + " %";
+        finalScoreText.text = "Score: " + score;
+        finalTargetsHitText.text = "Targets Hit: " + targetsHit + "/" + targetsAmmountInitial;
+        finalShotsFiredText.text = "Shots Fired: " + shotsFired;
+        finalAccuracyText.text = "Accuracy: " + accuracy.ToString("N2") + " %";
+        finalCurrentTimeText.text = "Time: -";
     }
 
     private void PrintTime()
